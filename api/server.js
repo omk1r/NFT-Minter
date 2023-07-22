76975f5dc4dbf73b04fde8f8647c9a06f4358b93
Omkar <jadhavpatil2999@gmail.com>
2023-07-22 23:50:01 +0530

    Final

--- api/server.js
+++ api/server.js
@@ -16,13 +16,64 @@ const upload = multer({
 const starton = axios.create({
   baseURL: "https://api.starton.io/v3",
   headers: {
-    "x-api-key": "sk_live_d970248c-d073-4c3e-9452-01ab6ffa4185",
+    "x-api-key": "sk_live_315e9974-8d0b-4213-9890-0ae2cdca4592",
   },
 });
 
 app.post("/upload", cors(), upload.single("file"), async (req, res) => {
-  let data = FormData();
+  let data = new FormData();
   const blob = new Blob([req.file.buffer], { type: req.file.mimetype });
-  data.append("file", blob, { filename: req.file.originalname });
+  data.append("file", blob, { filename: req.file.originalnam });
   data.append("isSync", "true");
+
+  async function uploadImageOnIpfs() {
+    const ipfsImg = await starton.post("/ipfs/file", data, {
+      headers: {
+        "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+      },
+    });
+    return ipfsImg.data;
+  }
+  async function uploadMetadataOnIpfs(imgCid) {
+    const metadataJson = {
+      name: `A Wonderful NFT`,
+      description: `Probably the most awesome NFT ever created !`,
+      image: `ipfs://ipfs/${imgCid}`,
+    };
+    const ipfsMetadata = await starton.post("/ipfs/json", {
+      name: "My NFT metadata Json",
+      content: metadataJson,
+      isSync: true,
+    });
+    return ipfsMetadata.data;
+  }
+
+  const SMART_CONTRACT_NETWORK = "polygon-mumbai";
+  const SMART_CONTRACT_ADDRESS = "0xa26b43Dcc4Fde42e4c2C485ec9401a22D4670584";
+  const WALLET_IMPORTED_ON_STARTON =
+    "0x249fE2d1222ae5056a66F1D1eC12A5B34bBa08B3";
+  async function mintNFT(receiverAddress, metadataCid) {
+    const nft = await starton.post(
+      `/smart-contract/${SMART_CONTRACT_NETWORK}/${SMART_CONTRACT_ADDRESS}/call`,
+      {
+        functionName: "mint",
+        signerWallet: WALLET_IMPORTED_ON_STARTON,
+        speed: "low",
+        params: [receiverAddress, metadataCid],
+      }
+    );
+    return nft.data;
+  }
+  const RECEIVER_ADDRESS = "0x9A516b3AB5114ad549B67a1D3A17cB587aF8aee8";
+  const ipfsImgData = await uploadImageOnIpfs();
+  const ipfsMetadata = await uploadMetadataOnIpfs(ipfsImgData.cid);
+  const nft = await mintNFT(RECEIVER_ADDRESS, ipfsMetadata.cid);
+  console.log(nft);
+  res.status(201).json({
+    transactionHash: nft.transactionHash,
+    cid: ipfsImgData.cid,
+  });
+});
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
